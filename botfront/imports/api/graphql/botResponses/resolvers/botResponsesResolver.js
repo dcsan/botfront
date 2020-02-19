f78d58d7987b420bdb1ea91b667f35578257120a
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-02-19 14:48:48 -0500

    chore: removes dead code for response added subscription

--- botfront/imports/api/graphql/botResponses/resolvers/botResponsesResolver.js
+++ botfront/imports/api/graphql/botResponses/resolvers/botResponsesResolver.js
@@ -14,18 +14,11 @@ import {
 const { PubSub, withFilter } = require('apollo-server-express');
 
 const pubsub = new PubSub();
-const RESPONSE_ADDED = 'RESPONSE_ADDED';
 const RESPONSES_MODIFIED = 'RESPONSES_MODIFIED';
 const RESPONSE_DELETED = 'RESPONSE_DELETED';
 
 export default {
     Subscription: {
-        botResponseAdded: {
-            subscribe: withFilter(
-                () => pubsub.asyncIterator([RESPONSE_ADDED]),
-                (payload, variables) => payload.projectId === variables.projectId,
-            ),
-        },
         botResponsesModified: {
             subscribe: withFilter(
                 () => pubsub.asyncIterator([RESPONSES_MODIFIED]),
@@ -83,7 +76,6 @@ export default {
                 projectId: args.projectId,
                 botResponsesModified: response,
             });
-            pubsub.publish(RESPONSE_ADDED, { projectId: args.projectId, botResponseAdded: args.response });
             return { success: !!response.id };
         },
         async createResponses(_, args, __) {
